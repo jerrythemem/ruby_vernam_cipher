da24a510cbf3638e63d5583704f067beeda16b72
mingetty <jerrythecloud@icloud.com>
2020-10-21 16:50:31 +0300

    Add string support, modify console output

--- decode.rb
+++ decode.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
 def decode(result, mask)
-  result.to_i(2) ^ mask.to_i(2)
+  puts 'Base is string or number?'
+  answer = gets.chomp
+  if answer == 'string'
+    base_byte = result.to_i(2) ^ mask.to_i(2)
+    decode_str(base_byte)
+  elsif answer == 'number'
+    result.to_i(2) ^ mask.to_i(2)
+  end
+end
+
+def decode_str(base_byte)
+  base_chr = base_byte.to_s(2).split ''
+  base = ''
+  sample = ''
+  (0..base_chr.length - 1).each do |i|
+    sample += base_chr[i].to_s
+    if sample.length == 7
+      base += sample.to_i(2).to_i.chr
+      sample = ''
+    end
+  end
+  base
 end
